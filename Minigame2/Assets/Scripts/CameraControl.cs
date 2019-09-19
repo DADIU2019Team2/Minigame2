a2870ae7b79f8185b50d68106776afea3f93b93f
Katerina <kkourk15@student.aau.dk>
2019-09-19 14:49:18 +0200

    added two test scenes

---
+++ Minigame2/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform targetTransform;
+    Vector3 temp = new Vector3();
+    
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        temp.x = targetTransform.position.x;
+        temp.y = this.transform.position.y;
+        temp.z = this.transform.position.z;
+        this.transform.position = temp;
+    }
+}
