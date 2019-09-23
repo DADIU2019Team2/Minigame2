b9ac94fd293911b2c169b40bae619813a52db86c
LasseAafeldt <superlla@hotmail.com>
2019-09-23 16:39:35 +0200

    made beginning of damageble object scriptable object and component

---
+++ Minigame2/Assets/Scripts/ScriptableObjectScripts/DamagebleObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Damageble object", menuName = "ScriptableObject/Damageble object")]
+public class DamagebleObject : ScriptableObject
+{
+    public int hp = 20;
+    public int dmgTaken = 5;
+    public string tagOfHarmfulObjects = "Write Tag here";
+}
