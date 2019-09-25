2945f04aa0978a4aa70971c3011bb2874ff1becf
AndersHansen93 <31648459+AndersHansen93@users.noreply.github.com>
2019-09-25 14:37:12 +0200

    Scene/death tranisition text logic
    logic for showing/disabling a ui with text for death and level end implemented

---
+++ Minigame2/Assets/Scripts/UI scripts/TransitionText.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class TransitionText : MonoBehaviour
+{
+    private GameObject currentTranistionObject;
+    public GameObject[] textTranistionArr;
+    public GameObject deathText;
+    public void ActivateTransitionText()
+    {
+        Time.timeScale = 0;
+        if (currentTranistionObject != null)
+        {
+            currentTranistionObject.SetActive(false);
+        }
+
+        textTranistionArr[SceneManager.GetActiveScene().buildIndex+1].SetActive(true);
+        currentTranistionObject = textTranistionArr[SceneManager.GetActiveScene().buildIndex];
+        Component[] gui = currentTranistionObject.GetComponentsInChildren<AutoTranslator>();
+        foreach (AutoTranslator item in gui)
+        {
+            item.OnLanguageChange();
+        }
+    }
+    public void ActivateDeathText()
+    {
+        Time.timeScale = 0;
+        if (currentTranistionObject != null)
+        {
+            currentTranistionObject.SetActive(false);
+        }
+        deathText.SetActive(true);
+        currentTranistionObject = deathText;
+        Component[] gui = currentTranistionObject.GetComponentsInChildren<AutoTranslator>();
+        foreach (AutoTranslator item in gui)
+        {
+            item.OnLanguageChange();
+        }
+    }
+
+    public void DeactivateText()
+    {
+        if (currentTranistionObject != null)
+        {
+            currentTranistionObject.SetActive(false);
+        }
+        Time.timeScale = 1;
+    }
+}
