8816d5c54ee3f2ead6e4526ff2d3fe74816eb69b
AndersHansen93 <31648459+AndersHansen93@users.noreply.github.com>
2019-09-26 11:35:17 +0200

    Portal Script

---
+++ Minigame2/Assets/Scripts/Portals.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portals : MonoBehaviour
+{
+    public static float teleportCooldown = 1f;
+    public static float entryTime = 0f;
+    public GameObject Exit;
+    public GameObject monster;
+
+    private void Start()
+    {
+        entryTime = Time.time;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (entryTime + teleportCooldown < Time.time)
+        {
+            Debug.Log("teleporting");
+            teleport(other.gameObject);
+        }
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (entryTime + teleportCooldown < Time.time)
+        {
+            Debug.Log("teleporting");
+            teleport(other.gameObject);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (entryTime + teleportCooldown < Time.time)
+        {
+            Debug.Log("teleporting");
+            teleport(collision.gameObject);
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (entryTime + teleportCooldown < Time.time)
+        {
+            Debug.Log("teleporting");
+            teleport(collision.gameObject);
+        }
+    }
+
+    private void teleport(GameObject target)
+    {
+        Transform parent = target.transform.parent;
+        Transform child = target.transform;
+        
+        while (parent != null)
+        {
+            child = parent;
+            parent = parent.parent;
+        }
+        Debug.Log(child.name);
+
+        child.GetComponent<CharacterController>().enabled = false;
+        child.transform.position= Exit.transform.position;
+
+        child.GetComponent<CharacterController>().enabled = true;
+        entryTime = Time.time;
+
+    }
+
+
+
+}
