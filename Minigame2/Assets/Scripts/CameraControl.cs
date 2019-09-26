3a3607604087d639ac94df5f35f2187b6e84dec1
Rutaceae <jamiemoeller@yahoo.dk>
2019-09-26 14:50:13 +0200

    Camera movement in beginning of level (after canvas fadeout transition) working

--- Minigame2/Assets/Scripts/CameraControl.cs
+++ Minigame2/Assets/Scripts/CameraControl.cs
@@ -4,26 +4,62 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+
+
     public Transform targetTransform;
     Vector3 temp = new Vector3();
 
+    public float startOfLevelTransitionTime = 3;
+    private float timeSinceLevelStarted = 0f;
+
+    private Vector3 bodyPosition;
+    private Vector3 initialPosition;
+    [SerializeField]
     private bool isStartOfLevel = true;
+    private bool hasCanvasFadedOut = false;
+
+
 
     private void Start()
     {
         isStartOfLevel = true;
+        initialPosition = transform.position;
         targetTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
     }
 
-    
 
-
-    // Update is called once per frame
-    void LateUpdate()
+    // Update is called once per framee
+    private void LateUpdate()
     {
-        temp.x = targetTransform.position.x;
-        temp.y = targetTransform.position.y;
-        temp.z = this.transform.position.z;
-        this.transform.position = temp;
+        if (!hasCanvasFadedOut)
+        {
+            return;
+        }
+
+        float zPos = transform.position.z;
+        float t = timeSinceLevelStarted / startOfLevelTransitionTime;
+        Debug.Log("Time: " + t);
+        isStartOfLevel = (t <= 0.99f);
+        if (isStartOfLevel)
+        {
+            Vector3 newPosition = Vector3.Lerp(initialPosition, targetTransform.position, t);
+            newPosition = new Vector3(newPosition.x, newPosition.y, zPos);
+            transform.position = newPosition;
+        }
+        else
+        {
+            FindObjectOfType<Gravity>().gameObject.SetActive(true);
+            temp.x = targetTransform.position.x;
+            temp.y = targetTransform.position.y;
+            temp.z = this.transform.position.z;
+            this.transform.position = temp;
+        }
+        timeSinceLevelStarted += Time.deltaTime;
+    }
+
+    public void CanvasFadedOutEventResponse()
+    {
+        hasCanvasFadedOut = true;
     }
 }
