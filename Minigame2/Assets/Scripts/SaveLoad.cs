fbd57562e79bfd3c84c844907b175c988355c2c3
AndersHansen93 <31648459+AndersHansen93@users.noreply.github.com>
2019-09-26 14:30:49 +0200

    Save Load Functionality

---
+++ Minigame2/Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.SceneManagement;
+
+public static class SaveLoad
+{
+    public static Game saveGame = new Game();
+
+    public static void Save()
+    {
+        Debug.Log("SAVING");
+        saveGame = Game.current;
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/savedGames.gd");
+        bf.Serialize(file, SaveLoad.saveGame);
+        file.Close();
+    }
+    public static void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/savedGames.gd"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+            SaveLoad.saveGame = (Game)bf.Deserialize(file);
+            file.Close();
+        }
+        Debug.Log("Continuing from level : "+SceneManager.GetSceneByBuildIndex(saveGame.lastLevelBeaten).name.ToString());
+        Game.current = saveGame;
+    }
+}
---
+++ Minigame2/Assets/Scripts/buttonSaveFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonSaveFunctions : MonoBehaviour
+{
+    public void Save()
+    {
+        SaveLoad.Save();
+    }
+    public void Load()
+    {
+        SaveLoad.Load();
+    }
+}
