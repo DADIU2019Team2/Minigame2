775a081ac3bfa28770bb9758f41e2c5c9e50a182
LasseAafeldt <superlla@hotmail.com>
2019-09-26 15:44:35 +0200

    input temp icon and made fade transitions

--- Minigame2/Assets/Scripts/UI scripts/TransitionText.cs
+++ Minigame2/Assets/Scripts/UI scripts/TransitionText.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-[RequireComponent(typeof(LoadLevel))]
+[RequireComponent(typeof(LoadLevel),typeof(CanvasGroup))]
 public class TransitionText : MonoBehaviour
 {
-
-    private GameObject currentTranistionObject;
     public GameObject[] textTranistionArr;
     public GameObject deathText;
+
+    [SerializeField] private float transitionFadeTime = 2f;
+    private CanvasGroup canvasGroup;
+    private GameObject currentTranistionObject;
+
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
     public void ActivateTransitionText()
     {
         Time.timeScale = 0;
+        StartCoroutine(fadeIn(canvasGroup, transitionFadeTime));
         if (currentTranistionObject != null)
         {
             currentTranistionObject.SetActive(false);
@@ -29,6 +37,7 @@ public class TransitionText : MonoBehaviour
     public void ActivateDeathText()
     {
         Time.timeScale = 0;
+        StartCoroutine(fadeIn(canvasGroup, transitionFadeTime));
         if (currentTranistionObject != null)
         {
             currentTranistionObject.SetActive(false);
@@ -44,6 +53,7 @@ public class TransitionText : MonoBehaviour
 
     public void DeactivateDeathText()
     {
+        StartCoroutine(fadeOut(canvasGroup, transitionFadeTime));
         if (currentTranistionObject != null)
         {
             currentTranistionObject.SetActive(false);
@@ -53,11 +63,43 @@ public class TransitionText : MonoBehaviour
     }
     public void DeactivateLevelText()
     {
+        StartCoroutine(fadeOut(canvasGroup, transitionFadeTime));
         if (currentTranistionObject != null)
         {
             currentTranistionObject.SetActive(false);
         }
         Time.timeScale = 1;
         this.gameObject.GetComponent<LoadLevel>().loadNextLevel();
+    }
+
+    IEnumerator fadeIn(CanvasGroup _canvas, float fadeTime)
+    {
+        float currentTime = 0f;
+        _canvas.alpha = 0f; //make sure it's "off" from the start
+        //Debug.Log("Am i called more than once?");
+
+        while(_canvas.alpha < 1)
+        {
+            //Debug.Log("Does this happen alot?");
+            float targetAlpha = currentTime/fadeTime;
+            _canvas.alpha = targetAlpha;
+            yield return new WaitForSecondsRealtime(0);//waits for next frame
+            currentTime += Time.unscaledDeltaTime;
+        }
+    }
+    IEnumerator fadeOut(CanvasGroup _canvas, float fadeTime)
+    {
+        float currentTime = 0f;
+        _canvas.alpha = 1f; //make sure it's "off" from the start
+        //Debug.Log("Am i called more than once?");
+
+        while (_canvas.alpha > 0)
+        {
+            //Debug.Log("Does this happen alot?");
+            float targetAlpha = 1 - (currentTime / fadeTime);
+            _canvas.alpha = targetAlpha;
+            yield return new WaitForSecondsRealtime(0);//waits for next frame
+            currentTime += Time.unscaledDeltaTime;
+        }
     }
 }
