f6788d51e35d68858fc9a29d7256de8c18deb4f5
fredrig <neumannze@hotmail.com>
2019-09-30 10:52:24 +0200

    Added sounds to animations

---
+++ Minigame2/Assets/Scripts/WWise Custom Scripts/HumanTransformEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanTransformEvent : MonoBehaviour
+{
+    public string EnterSoulSound = "HumanTransform";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AkSoundEngine.RegisterGameObj(gameObject);
+    }
+    
+    void HumanTransform()    
+    {    
+        AkSoundEngine.PostEvent(EnterSoulSound, gameObject);
+    }
+}
