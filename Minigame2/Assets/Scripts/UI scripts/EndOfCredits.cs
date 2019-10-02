eb99064d39dbd58987adfb0d3ec0d1211e23b576
atange-filmskolen <52198175+atange-filmskolen@users.noreply.github.com>
2019-10-02 11:37:28 +0200

    end credits scene added

---
+++ Minigame2/Assets/Scripts/UI scripts/EndOfCredits.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(LoadLevel))]
+public class EndOfCredits : MonoBehaviour
+{
+    public AnimationClip animationToWAitFor;
+    private LoadLevel loadLevel;
+    Animation anim;
+    private void Start()
+    {
+        loadLevel = GetComponent<LoadLevel>();
+        StartCoroutine(endOfCredits());
+    }
+
+    IEnumerator endOfCredits()
+    {
+        yield return new WaitForSeconds(animationToWAitFor.length + 0.2f);
+
+        loadLevel.loadNextLevel();
+    }
+}
